495f03170d83891bfea7ae6c79967348ba910972
Keerthanajayagopi <141701245+Keerthanajayagopi@users.noreply.github.com>
2024-05-23 15:22:51 +0530

    Add files via upload

---
+++ portscript.js
@@ -0,0 +1,9 @@
+// const text=document.querySelector(".sec-text");
+// const textLoad=()=>{
+//     setTimeout(()=>{
+//         text.textContent="Frontend Developer !";
+//     },0);
+//     setTimeout(()=>{
+//         text.textContent="Web Developer !";
+//     },4000);
+// }
